a9e1255fbb47845858c352f8e616fdd70994bb19
Cheese <>
2021-05-03 16:34:17 +0000

    owo

--- modules/nsfwneko.js
+++ modules/nsfwneko.js
@@ -7,7 +7,7 @@ function neko(message) {
     const index = Math.floor(Math.random() * posts.length)
     const post = posts[index]
     const url = booru.url(post.file_url)
-    fs.readFile('./catfacts.txt', function(err, data) {
+    fs.readFile('../catfacts.txt', function(err, data) {
       if (err) throw err;
       var lines = data.toString().split('\n');
       message.channel.send(lines[Math.floor(Math.random() * lines.length)], { files: [url.href] }).catch(err => 1 + 1)
